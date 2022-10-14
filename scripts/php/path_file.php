d28417d53d1ada1ad8c0261ba5d6a494ab91b022
Lucas García <lucas@codeccoop.org>
2022-10-14 19:34:37 +0200

    Add deny access to private folders

--- scripts/php/path_file.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$path = '/var/log/apache/error.log';
-
-echo print_r(array_slice(explode('/', $path), 1));
--- scripts/php/style.php
+++
@@ -1,5 +0,0 @@
-<?php
-$path = 'https://api.github.com/repos/lucasgarciabaro/jekyll-jam/actions/runs/3178709478/artifacts';
-preg_match('/(?<=\/)[0-9]+(?=\/)/', $path, $matches);
-
-echo print_r($matches);
