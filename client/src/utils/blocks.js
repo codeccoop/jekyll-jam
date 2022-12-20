248820387bc714927a2e65c4e6f7451c33e02dc1
Lucas García <lucas@codeccoop.org>
2022-12-20 08:48:32 +0100

    feat: block marks

--- client/src/utils/blocks.js
+++ client/src/utils/blocks.js
@@ -1,23 +1,40 @@
-export function renderBlock(block, node) {
+function commentWrap(meta, dom) {
+  let node = dom.children[0];
+  const openTag = new Comment(`\n[VOCERO-BLOCK]\n${meta}\n`);
+  const closeTag = new Comment("[/VOCERO-BLOCK]");
+  dom.insertBefore(openTag, node);
+  dom.appendChild(closeTag);
+  return dom;
+}
+
+function renderBlock(block, node) {
+  const parser = document.createElement("template");
   const attributes = Object.fromEntries(
     Object.keys(node.attributes).map((idx) => {
       return [node.attributes[idx].name, node.attributes[idx].value];
     })
   );
-  return block.fn(attributes);
+
+  parser.innerHTML = block.fn(attributes);
+  const dom = parser.content;
+
+  return commentWrap(node.outerHTML, dom);
 }
 
 export function renderBlocks(html, blocks) {
   if (!html) return "";
   const doc = new DOMParser().parseFromString(html, "text/html");
-  const parser = document.createElement("template");
 
   for (let block of blocks) {
     const nodes = doc.getElementsByTagName(block.name.toLowerCase());
     for (let i = 0; i < nodes.length; i++) {
+      debugger;
       const node = nodes[i];
-      parser.innerHTML = renderBlock(block, node);
-      node.insertAdjacentElement("beforebegin", parser.content.children[0]);
+      const dom = renderBlock(block, node);
+      for (let child of dom.childNodes) {
+        node.parentElement.insertBefore(child, node);
+        // node.insertAdjacentElement("beforebegin", content.children[0]);
+      }
       node.parentElement.removeChild(node);
     }
   }
