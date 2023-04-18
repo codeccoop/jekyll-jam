ae408b860fa4b1c5778b562aa828e012c4566cf9
Lucas García <lucas@codeccoop.org>
2023-04-18 18:24:07 +0200

    feat: update rs/blocks.php

--- rs/blocks.php
+++ rs/blocks.php
@@ -21,22 +21,24 @@ try {
         level: "block",
         args: ["url", "width", "height"],
         selfClosed: true,
-        fn: ({ React, src, width = "500px", height = "auto" }, ref) => React.createElement("video", {src, width, height})
+        // fn: ({ React, src, width = "500px", height = "auto" }, ref) => React.createElement("video", {src, width, height}),
+        fn: ({ React, src, width = "500px", height = "auto" }) => React.createElement("video", {src, width, height}),
     }, {
         family: "layout",
         name: "Columns",
         level: "block",
         args: [],
         selfClosed: false,
-        fn: ({ React, children }, ref) => {
-            return React.createElement("div", {ref,className:"columns",style:{display:"flex",width:"100%"}}, children);
-    }}, {
+        // fn: ({ React, children }, ref) => React.createElement("div", {ref,className:"columns",style:{display:"flex",width:"100%"}}, children),
+        fn: ({ React, children }) => React.createElement("div", {className:"columns",style:{display:"flex",width:"100%"}}, children),
+    }, {
         family: "layout",
         name: "Column",
         level: "block",
         args: ["span", "gutter"],
         selfClosed: false,
-        fn: ({ React, span = 1, gutter = "0", children }, ref) => React.createElement("div", {className:"column",style:{display:"block",flex:span ? span : 1,padding:gutter ? gutter : "0"}}, children)
+        // fn: ({ React, span = 1, gutter = "0", children }, ref) => React.createElement("div", {className:"column",style:{display:"block",flex:span ? span : 1,padding:gutter ? gutter : "0"}}, children)
+        fn: ({ React, span = 1, gutter = "0", children }) => React.createElement("div", {className:"column",style:{display:"block",flex:span ? span : 1,padding:gutter ? gutter : "0"}}, children)
     }]';
     /* header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found'); */
     /* header('Content-Type: application/json'); */
